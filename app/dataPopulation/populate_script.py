5a38597e1c2aec99cc9d83c54b50d21e452ba6a4
vsarode <vitthal.sarode1@gmail.com>
2019-03-31 16:08:58 +0530

    data population breaking fix

--- app/dataPopulation/populate_script.py
+++ app/dataPopulation/populate_script.py
@@ -71,7 +71,10 @@ def populate_subjects(con):
     cursor = con.cursor()
     for s in sub_name:
         print s
-        cursor.execute("insert into subject values(%s,%s)", (s['code'], s['name'],))
+        try:
+            cursor.execute("insert into subject values(%s,%s)", (s['code'], s['name'],))
+        except:
+            pass
     con.commit()
     print "Subject populated !!"
 
@@ -80,7 +83,10 @@ def populate_college(con):
     cursor = con.cursor()
     for s in college:
         print s
-        cursor.execute("insert into college values(%s,%s,%s)", (s['code'], s['name'],s['address'],))
+        try:
+            cursor.execute("insert into college values(%s,%s,%s)", (s['code'], s['name'],s['address'],))
+        except:
+            pass
     con.commit()
     print "College populated !!"
 
@@ -98,121 +104,124 @@ def populate_data(con):
         truncate_table(con)
         populate_subjects(con)
         populate_college(con)
-        for row in csvreader:
-            subject_data = row[:19]
-            id = id + 1
+        try:
+            for row in csvreader:
+                subject_data = row[:19]
+                id = id + 1
 
-            # print row
-            # clg_data = [row[4]]
-            # clg_data.extend(row[5:7])
-            # print "college: ", clg_data
-            #
-            # query1 = "insert into college values(%s, %s, %s)"
-            # res1 = cursor.execute(query1, clg_data)
-            # con.commit()
-            # # print res1
-
-
-
-            # print row[:5]
-            student_data = row[:5]
-            print "student:", student_data
-            query = "insert into student values(%s, %s, %s, %s, %s)"
-            res = cursor.execute(query, student_data)
-            con.commit()
-            # print res
-            print row
-            subject_data = [row[5]]
-            subject_data.append(row[0])
-            subject_data.extend(row[6:8])
-            print "subject1:", subject_data
-            query2 = "insert into subMarks(subCode,prnNo,obtainMarks,totalMarks) values (%s,%s,%s,%s)"
-            res = cursor.execute(query2, subject_data)
-            con.commit()
-
-            # subject_data1.append(id)
-            # print subject_data1
-
-            subject_data2 = [row[8]]
-            subject_data2.append(row[0])
-            subject_data2.extend(row[9:11])
-
-            print "subject2", subject_data2
-            query3 = "insert into subMarks (subCode,prnNo,obtainMarks,totalMarks) value(%s, %s, %s,%s)"
-            res = cursor.execute(query3, subject_data2)
-            con.commit()
-
-            subject_data3 = [row[11]]
-            subject_data3.append(row[0])
-            subject_data3.extend(row[12:14])
-            print "subject3", subject_data3
-            query4 = "insert into subMarks (subCode,prnNo,obtainMarks,totalMarks) value(%s, %s, %s,%s)"
-            res = cursor.execute(query4, subject_data3)
-            con.commit()
-
-            subject_data4 = [row[14]]
-            subject_data4.append(row[0])
-            subject_data4.extend(row[15:17])
-            print "subject4", subject_data4
-            query5 = "insert into subMarks (subCode,prnNo,obtainMarks,totalMarks) value(%s, %s, %s,%s)"
-            res = cursor.execute(query5, subject_data4)
-            con.commit()
-
-
-            result_data = [row[0]]
-            result_data.append(row[17])
-            result_data.extend(row[18:20])
-            # result_data.append(id)
-            print "result", result_data
-            query6 = "insert into result (prnNo, totalMarks, obtainMarks, sem) value(%s, %s, %s,%s )"
-            res = cursor.execute(query6, result_data)
-            con.commit()
-
-
-            # query = "insert into subject value(%s, %s, %s, %s, %s)"
-            # res = cursor.execute(query, subject_data)
-            # con.commit()
-            # print res
-            #
-            # college_data = row[4:7]
-            # query = "insert into college value(%s, %s, %s)"
-            # res = cursor.execute(query, college_data)
-            # con.commit()
-            # print res
-            #
-            # subject_data = row[7:9]
-            # query = "insert into subject value(%s, %s)"
-            # res = cursor.execute(query, subject_data)
-            # con.commit()
-            # print res
-            #
-            # _data = row[7:9]
-            # query = "insert into subject value(%s, %s)"
-            # res = cursor.execute(query, subject_data)
-            # con.commit()
-            # print res
-
-            # break
-
-            # extracting each data row one by one
-            # for row in csvreader:
-            # print row[7:19]
+                # print row
+                # clg_data = [row[4]]
+                # clg_data.extend(row[5:7])
+                # print "college: ", clg_data
+                #
+                # query1 = "insert into college values(%s, %s, %s)"
+                # res1 = cursor.execute(query1, clg_data)
+                # con.commit()
+                # # print res1
 
 
 
-            #  student_data = row[:12]
-            # query = "insert into student value(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            # res = cursor.execute(query, student_data)
-            # con.commit()
-            # print res
+                # print row[:5]
+                student_data = row[:5]
+                print "student:", student_data
+                query = "insert into student values(%s, %s, %s, %s, %s)"
+                res = cursor.execute(query, student_data)
+                con.commit()
+                # print res
+                print row
+                subject_data = [row[5]]
+                subject_data.append(row[0])
+                subject_data.extend(row[6:8])
+                print "subject1:", subject_data
+                query2 = "insert into subMarks(subCode,prnNo,obtainMarks,totalMarks) values (%s,%s,%s,%s)"
+                res = cursor.execute(query2, subject_data)
+                con.commit()
 
-            # break
+                # subject_data1.append(id)
+                # print subject_data1
 
-            # get total number of rows
-        student_data = row[5]
-        # clg_data=row[3]
+                subject_data2 = [row[8]]
+                subject_data2.append(row[0])
+                subject_data2.extend(row[9:11])
 
-        # print("Total no. of rows: %d" % (csvreader.line_num))
+                print "subject2", subject_data2
+                query3 = "insert into subMarks (subCode,prnNo,obtainMarks,totalMarks) value(%s, %s, %s,%s)"
+                res = cursor.execute(query3, subject_data2)
+                con.commit()
+
+                subject_data3 = [row[11]]
+                subject_data3.append(row[0])
+                subject_data3.extend(row[12:14])
+                print "subject3", subject_data3
+                query4 = "insert into subMarks (subCode,prnNo,obtainMarks,totalMarks) value(%s, %s, %s,%s)"
+                res = cursor.execute(query4, subject_data3)
+                con.commit()
+
+                subject_data4 = [row[14]]
+                subject_data4.append(row[0])
+                subject_data4.extend(row[15:17])
+                print "subject4", subject_data4
+                query5 = "insert into subMarks (subCode,prnNo,obtainMarks,totalMarks) value(%s, %s, %s,%s)"
+                res = cursor.execute(query5, subject_data4)
+                con.commit()
+
+
+                result_data = [row[0]]
+                result_data.append(row[17])
+                result_data.extend(row[18:20])
+                # result_data.append(id)
+                print "result", result_data
+                query6 = "insert into result (prnNo, totalMarks, obtainMarks, sem) value(%s, %s, %s,%s )"
+                res = cursor.execute(query6, result_data)
+                con.commit()
+
+
+                # query = "insert into subject value(%s, %s, %s, %s, %s)"
+                # res = cursor.execute(query, subject_data)
+                # con.commit()
+                # print res
+                #
+                # college_data = row[4:7]
+                # query = "insert into college value(%s, %s, %s)"
+                # res = cursor.execute(query, college_data)
+                # con.commit()
+                # print res
+                #
+                # subject_data = row[7:9]
+                # query = "insert into subject value(%s, %s)"
+                # res = cursor.execute(query, subject_data)
+                # con.commit()
+                # print res
+                #
+                # _data = row[7:9]
+                # query = "insert into subject value(%s, %s)"
+                # res = cursor.execute(query, subject_data)
+                # con.commit()
+                # print res
+
+                # break
+
+                # extracting each data row one by one
+                # for row in csvreader:
+                # print row[7:19]
+
+
+
+                #  student_data = row[:12]
+                # query = "insert into student value(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                # res = cursor.execute(query, student_data)
+                # con.commit()
+                # print res
+
+                # break
+
+                # get total number of rows
+            student_data = row[5]
+            # clg_data=row[3]
+
+            # print("Total no. of rows: %d" % (csvreader.line_num))
+        except:
+            pass
     cursor.close()
 
 def migrate(con):
